8d87de5cbdce9d93a05610d7a6f37f933d709d79
김재형 <francisggum@outlook.com>
2025-03-20 23:32:13 +0900

    코드 정리 및 오류 처리 개선: 불필요한 임포트 제거 및 데이터베이스 오류 로깅 추가

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -60,6 +60,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
         // Test it out:
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.error('Database Error:', error);
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
